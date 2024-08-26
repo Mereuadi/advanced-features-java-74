12385e82a7b9ee9ebe36948da1611703a640361b
Adrian Catalin <adrian.mereuta@gmail.com>
2024-08-26 19:13:13 +0300

    init exercise oop

---
+++ src/main/java/org/example/exercises/oop/Main.java
@@ -0,0 +1,16 @@
+package org.example.exercises.oop;
+
+/*
+    Create a multimedia player system where each multimedia item (audio, video) has its own methods to play and stop.
+    -clasa parinte MultimediaItem cu 2
+    =clase copil
+    Implement abstraction by defining an abstract class MultimediaItem with abstract methods for playing and stopping.
+    Create concrete classes Audio and Video that extend the MultimediaItem class and implement their specific methods for playing and stopping.
+*/
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
